998c305879f143fa7324cf4e29ad9288c969bca1
Anshul Mahajan <harshalsharma101@gmail.com>
2025-11-13 22:56:51 +0530

    adding setup.py file to the project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Anshul",
+    author_email="harshalsharma101@gmail.com",
+    packages=find_packages()
+)
